7da533a5bf8a5223bd1145fbabcc92049924a70c
YeaWon Kim <yeawon6566@gmail.com>
2025-03-13 22:06:41 +0900

    [level 2] Title: 행렬의 곱셈, Time: 8.67 ms, Memory: 91.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+        
+        for (int i = 0; i < arr1.length; i++) {
+            
+            for (int j = 0; j < arr2[0].length; j++) {
+                
+                int temp = 0;
+                for (int z = 0; z < arr2.length; z++) {
+                    temp += arr1[i][z] * arr2[z][j];
+                }
+                answer[i][j] = temp;
+            }
+            
+        }
+        
+        return answer;
+    }
+}
