3b98478ae87c1f2b7466eedc236994ed9eebba9e
YeaWon Kim <yeawon6566@gmail.com>
2025-09-23 19:45:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int[] dp = new int[11];
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        
+        for (int i=4; i<11; i++) {
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        }
+        
+        for (int i=0; i<T; i++) {
+            int n = Integer.parseInt(br.readLine());
+            
+            System.out.println(dp[n]);
+        }
+    }
+    
+}
