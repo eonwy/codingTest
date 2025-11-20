ad3d59b077cfd521c0ebb8b20b2bb86f740530cb
YeaWon Kim <yeawon6566@gmail.com>
2025-11-20 13:37:36 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 100 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static int[][] arr;
+    static boolean[][] visited;
+    
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        visited = new boolean[N][N];
+        
+        for (int i=0; i<N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j=0; j<N; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        boolean result = dfs(0, 0);
+        System.out.println(result ? "HaruHaru" : "Hing");
+    }
+    
+    public static boolean dfs(int x, int y) {
+        
+        // 범위 밖
+        if (x >= N || x < 0 || y >= N || y < 0) return false;
+        
+        // 이미 방문
+        if (visited[x][y]) return false;
+        
+        // 성공
+        if (x == N-1 && y == N-1) return true;
+        
+        visited[x][y] = true;
+        
+        int jump = arr[x][y];
+        
+        if (jump == 0) return false;
+        if (dfs(x+jump, y)) return true;
+        if (dfs(x, y+jump)) return true;
+        
+        return false;
+    }
+}
