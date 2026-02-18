812d3f4be2746f98ea3ca9513fb19e3635bb8472
YeaWon Kim <yeawon6566@gmail.com>
2026-02-18 23:03:40 +0900

    [Silver V] Title: 단어 정렬, Time: 456 ms, Memory: 23624 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        HashSet<String> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            set.add(br.readLine());
+        }
+
+        List<String> list = new ArrayList<>(set);
+
+        list.sort((a, b) -> {
+            if (a.length() == b.length()) {
+                return a.compareTo(b);
+            }
+            return a.length() - b.length();
+        });
+
+        for (String s : list) {
+            System.out.println(s);
+        }
+    }
+}
