d747af54867100cf50a6833bbd7c68b39af9a4d8
YeaWon Kim <yeawon6566@gmail.com>
2026-03-30 18:35:37 +0900

    [level 3] Title: 단속카메라, Time: 9.83 ms, Memory: 60.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] routes) {
+        int answer = 0;
+        
+        Arrays.sort(routes, (a, b) -> a[1] - b[1]);
+        
+        int camera = Integer.MIN_VALUE;
+        
+        for (int i = 0; i < routes.length; i++) {
+            if (camera < routes[i][0]) {
+                answer++;
+                camera = routes[i][1];
+            }
+        }
+
+        return answer;
+    }
+}
