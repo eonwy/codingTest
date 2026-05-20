9676d885b1f4f720deb4bceebdcdfa6bdb7eb8a4
Yeawon Kim <yeawon6566@gmail.com>
2026-05-20 23:15:06 +0900

    [level 1] Title: 카드 뭉치, Time: 0.02 ms, Memory: 62.5 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -5,10 +5,10 @@ class Solution {
         int index1 = 0;
         int index2 = 0;
         
-        for(String s : goal) {
-            if (index1 < cards1.length && s.equals(cards1[index1])) {
+        for (String g : goal) {
+            if (index1 < cards1.length && g.equals(cards1[index1])) {
                 index1++;
-            } else if (index2 < cards2.length && s.equals(cards2[index2])) {
+            } else if (index2 < cards2.length && g.equals(cards2[index2])) {
                 index2++;
             } else {
                 return "No";
