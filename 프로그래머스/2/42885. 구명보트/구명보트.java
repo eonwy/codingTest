b6619d46663669b639ea1ac0b9958b50189c7c4e
Yeawon Kim <yeawon6566@gmail.com>
2026-05-21 23:11:49 +0900

    [level 2] Title: 구명보트, Time: 6.73 ms, Memory: 59.5 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -2,21 +2,20 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] people, int limit) {
+        int cnt = 0;
+        int low = 0;
+        int high = people.length - 1;
         
         Arrays.sort(people);
         
-        int light = 0;
-        int heavy = people.length - 1;
-        int count = 0;
-        
-        while(light <= heavy) {
-            if (people[light] + people[heavy] <= limit) {
-                light++;
+        while(low <= high) {
+            if (people[low] + people[high] <= limit) {
+                low++;
             }
-            heavy--;
-            count++;
+            high--;
+            cnt++;
         }
         
-        return count;
+        return cnt;
     }
 }
