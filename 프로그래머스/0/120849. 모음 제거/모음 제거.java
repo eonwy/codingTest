4fd1b1b435d8e04efe6d5943e46290b29b95c2e5
Yeawon Kim <yeawon6566@gmail.com>
2026-05-29 23:57:35 +0900

    [level 0] Title: 모음 제거, Time: 0.04 ms, Memory: 63.6 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.java
@@ -1,21 +1,15 @@
+import java.util.*;
+
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        String[] except = new String[] {"a", "e", "i", "o", "u"};
+        StringBuilder answer = new StringBuilder();
         
-        for(int i=0; i<my_string.length(); i++){
-            int vowel = 0;
-            
-            for(int j=0; j<except.length; j++){
-                if(String.valueOf(my_string.charAt(i)).equals(except[j])){
-                    vowel++;
-                }
-            }
-            
-            if(vowel == 0){
-                answer += my_string.charAt(i);
+        for (int i = 0; i < my_string.length(); i++) {
+            if (!"aeiou".contains(String.valueOf(my_string.charAt(i)))) {
+                answer.append(my_string.charAt(i));
             }
         }
-        return answer;
+        
+        return answer.toString();
     }
 }
