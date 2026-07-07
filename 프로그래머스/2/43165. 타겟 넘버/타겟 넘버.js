7b74e06ac9488b5f9931989fb2e4b4433ea09b8b
Yeawon Kim <yeawon6566@gmail.com>
2026-07-08 00:32:02 +0900

    [level 2] Title: 타겟 넘버, Time: 7.99 ms, Memory: 49 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,21 +1,4 @@
-let answer = 0;
-
 function solution(numbers, target) {
-    answer = 0;
-    
-    dfs(numbers, target, 0, 0);
-    
+    var answer = 0;
     return answer;
-}
-
-function dfs(numbers, target, depth, result) {
-    if (depth === numbers.length) {
-        if (result === target) {
-            answer++;
-        }
-        return;
-    }
-    
-    dfs(numbers, target, depth+1, result+numbers[depth]);
-    dfs(numbers, target, depth+1, result-numbers[depth]);
 }
